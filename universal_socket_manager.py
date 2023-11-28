c6bd30b81f99f0fed7d1ed081aa3a718ecf9d579
Eetu <148777334+eetmie@users.noreply.github.com>
2023-11-28 17:15:43 +0200

    Update universal_socket_manager.py

--- universal_socket_manager.py
+++ universal_socket_manager.py
@@ -4,6 +4,7 @@ import datetime
 import threading
 import os
 
+from time import sleep
 
 # using these directly now. Add to __init__ if more flexibility needed
 from config import (file_path, local_addr, port, connect_addr, identification_number,
@@ -37,10 +38,9 @@ class MasiSocketManager:
 
     @staticmethod
     def clear_file():
-        # try:
         with open(file_path, 'wb'):
             pass
-        # except (no file error find this):
+        print("Cleared file!")
 
     @staticmethod
     def pack_data(data):
@@ -49,16 +49,10 @@ class MasiSocketManager:
 
     @staticmethod
     def print_bin_file(num_doubles):
-        format_str = endian_specifier + unix_format + data_format * num_doubles
-        total_size = struct.calcsize(format_str)
+        format_str = endian_specifier + unix_format + data_format * num_doubles + checksum_format
 
-        # Add checksum byte if not divisible by 2
-        if total_size % 2 != 0:
-            format_str += checksum_format
-
-        # Format string differs from recv_bytes with added timestamp
         print(f"File format is: {str(format_str)}")
-
+        sleep(2)
         data_size = struct.calcsize(format_str)
 
         if not os.path.exists(file_path):
@@ -109,7 +103,6 @@ class MasiSocketManager:
         if self.local_socket:
             self.local_socket.close()
             print("Socket closed successfully!")
-            # self.save_remaining_data()
         else:
             print("something wrong has happened somewhere")
 
@@ -197,12 +190,12 @@ class MasiSocketManager:
         self.data_buffer.clear()
         return True
 
-    def save_remaining_data(self):
+    def save_remaining_data(self, num_doubles):
         # If there's remaining data in the buffer, save it to file
         if self.data_buffer:
             with open(file_path, 'ab') as f:
                 for value in self.data_buffer:
-                    missing_values = outputs - (len(value) // 8 - 1)  # subtract 1 for the timestamp
+                    missing_values = num_doubles - (len(value) // 8 - 1)  # subtract 1 for the timestamp
                     value += struct.pack((endian_specifier + '{}' + data_format).format(missing_values), *([0.0] * missing_values))  # 0.0 doubles
                     f.write(value)
             self.data_buffer.clear()
