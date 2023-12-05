6582b350161b56444106be30da391f38cbdc16c8
Eetu <148777334+eetmie@users.noreply.github.com>
2023-12-05 14:52:28 +0200

    Add data record start / stop button

--- test_file.py
+++ test_file.py
@@ -2,8 +2,6 @@ import motionplatf_controls
 import universal_socket_manager
 from time import sleep, time
 
-simulation_mode = False
-
 # Frequency of the send/receive loop in Hz
 # 20Hz ~50ms
 # 60Hz ~17ms
@@ -11,11 +9,12 @@ simulation_mode = False
 loop_frequency = 20
 
 # init joysticks
-motionplatf_output = motionplatf_controls.DataOutput(simulation_mode=simulation_mode, decimals=3)
+motionplatf_output = motionplatf_controls.DataOutput(simulation_mode=False, decimals=3)
 
 
 # init socket
 manager = universal_socket_manager.MasiSocketManager()
+
 
 def setup():
     manager.clear_file()
@@ -26,7 +25,6 @@ def setup():
         return
     handshake_result = manager.handshake()
 
-
     if not handshake_result:
         print("could not make handshake!")
         return
@@ -34,17 +32,28 @@ def setup():
 
 
 def run():
+    record_flag = False
     while True:
         loop_start_time = time()
         # read values from joysticks
         joystick_data = motionplatf_output.read(combine=True)
+        rec_start_button = joystick_data[12]  # right stick top button
+        rec_stop_button = joystick_data[11]     # right stick bot button
+
+        if rec_start_button and not record_flag:
+            print("Started recording!")
+            record_flag = True
+        if rec_stop_button and record_flag:
+            print("Stopped recording!")
+            record_flag = False
 
         try:
             # send joystick data
             packed_sent_data = manager.send_data(joystick_data)
 
-            #add to buffer
-            manager.add_data_to_buffer(packed_sent_data)
+            if record_flag:
+                # add to buffer
+                manager.add_data_to_buffer(packed_sent_data)
 
             # Receive handshake
             keep_alive = manager.receive_data()
@@ -56,6 +65,7 @@ def run():
             time_elapsed = time() - loop_start_time
             sleep_time = max(0, (1.0 / loop_frequency) - time_elapsed)
             sleep(sleep_time)
+
         except (Exception, KeyboardInterrupt) as e:
             manager.save_remaining_data(num_doubles=20)
             print(e)
